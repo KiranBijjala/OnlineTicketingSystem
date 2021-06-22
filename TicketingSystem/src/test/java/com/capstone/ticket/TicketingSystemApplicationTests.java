d9b0184e6c4b63f647eaeb73bfdc82bbb1d5d4ea
Kiran Kumar Bijjala <kirankumar.bijjala@sjsu.edu>
2021-06-22 16:01:38 -0400

    New Files added

---
+++ TicketingSystem/src/test/java/com/capstone/ticket/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/main/java/com/capstone/ticket/model/Ticket.java
+++
@@ -1,5 +0,0 @@
-package com.capstone.ticket.model;
-
-public class Ticket {
-
-}
