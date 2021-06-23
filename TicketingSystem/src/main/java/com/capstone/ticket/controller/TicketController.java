a493d534047f0424e0cde1554e0ef6c5e584c284
AlejandroCSC <AlejandroCSC@protonmail.com>
2021-06-23 15:09:23 -0700

    Commented out code.

--- TicketingSystem/src/main/java/com/capstone/ticket/controller/TicketController.java
+++ TicketingSystem/src/main/java/com/capstone/ticket/controller/TicketController.java
@@ -1,6 +1,5 @@
 package com.capstone.ticket.controller;
 
-
 import com.capstone.ticket.model.Ticket;
 
 import com.capstone.ticket.model.User;
@@ -15,138 +14,141 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 public class TicketController {
 
-    @Autowired
-    TicketRepository ticketRepository;
+	@Autowired
+	TicketRepository ticketRepository;
 
-//    @PostMapping(value="/addticket", produces = {MediaType.APPLICATION_JSON_VALUE},consumes= {MediaType.APPLICATION_JSON_VALUE})
-//    public Ticket addTicket(@RequestBody Ticket ticket,@RequestParam Optional<String> passenger){
-//
-//        if(!passenger.isPresent()){
-//            return ticketRepository.save(ticket);
-//        }
-//        System.out.println(ticket.toString());
-//        System.out.println("Hi");
-//        StringBuilder sb = new StringBuilder();
-//        System.out.println(passenger.get());
-//        sb = sb.append(ticket.getName()).append(",").append(passenger.get());
-//        System.out.println("String: " + sb.toString());
-//        ticket.setName(sb.toString());
-//    	return ticketRepository.save(ticket);
-//    	
-//    }
+	// @PostMapping(value="/addticket", produces =
+	// {MediaType.APPLICATION_JSON_VALUE},consumes=
+	// {MediaType.APPLICATION_JSON_VALUE})
+	// public Ticket addTicket(@RequestBody Ticket ticket,@RequestParam
+	// Optional<String> passenger){
+	//
+	// if(!passenger.isPresent()){
+	// return ticketRepository.save(ticket);
+	// }
+	// System.out.println(ticket.toString());
+	// System.out.println("Hi");
+	// StringBuilder sb = new StringBuilder();
+	// System.out.println(passenger.get());
+	// sb = sb.append(ticket.getName()).append(",").append(passenger.get());
+	// System.out.println("String: " + sb.toString());
+	// ticket.setName(sb.toString());
+	// return ticketRepository.save(ticket);
+	//
+	// }
 
-//    @GetMapping(value="/gettickets", produces = {MediaType.APPLICATION_JSON_VALUE},consumes= {MediaType.APPLICATION_JSON_VALUE})
-//    public List<Ticket> getTickets(){
-//
-//        System.out.println("Inside Get all Tickets");
-//        return ticketRepository.findAll();
-//
-//    }
-    
-//    @RequestMapping("/")
-//	public ModelAndView viewHomePage() {
-//		List<Ticket> listTickets = ticketRepository.findAll();
-//		
-//		System.out.println(listTickets);
-//		ModelAndView model = new ModelAndView("index");
-//		
-//		model.addObject("listTickets",listTickets);
-//		return model;
-//		
-//    }
-    
-    @RequestMapping("/gettickets/{name}")
+	// @GetMapping(value="/gettickets", produces =
+	// {MediaType.APPLICATION_JSON_VALUE},consumes=
+	// {MediaType.APPLICATION_JSON_VALUE})
+	// public List<Ticket> getTickets(){
+	//
+	// System.out.println("Inside Get all Tickets");
+	// return ticketRepository.findAll();
+	//
+	// }
+
+	// @RequestMapping("/")
+	// public ModelAndView viewHomePage() {
+	// List<Ticket> listTickets = ticketRepository.findAll();
+	//
+	// System.out.println(listTickets);
+	// ModelAndView model = new ModelAndView("index");
+	//
+	// model.addObject("listTickets",listTickets);
+	// return model;
+	//
+	// }
+
+	@RequestMapping("/gettickets/{name}")
 	public ModelAndView HomePage(@PathVariable String name) {
 		List<Ticket> listTickets = ticketRepository.findByName(name);
 		System.out.println(listTickets);
 		ModelAndView model = new ModelAndView("index");
-		
-		model.addObject("listTickets",listTickets);
+
+		model.addObject("listTickets", listTickets);
 		return model;
-		
-    }
-    
-    
-    
-    @RequestMapping("/addticket")
+
+	}
+
+	@RequestMapping("/addticket")
 	public ModelAndView addTicket() {
 		Ticket ticket = new Ticket();
 		ModelAndView model = new ModelAndView("new_ticket");
 		model.addObject("ticket", ticket);
-		
+
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ModelAndView saveTicket(@ModelAttribute("ticket") Ticket ticket, @RequestParam String passengers) {
 		String name = ticket.getName();
-		
+
 		Optional<String> passenger = Optional.ofNullable(passengers);
-		if(!passenger.isPresent()){
-          ticketRepository.save(ticket);
+		if (!passenger.isPresent()) {
+			ticketRepository.save(ticket);
 		}
-//	      System.out.println(ticket.toString());
-//	      System.out.println("Hi");
-//	      StringBuilder sb = new StringBuilder();
-//	      System.out.println(passenger.get());
-//	      sb = sb.append(ticket.getName()).append(",").append(passenger.get());
-//	      System.out.println("String: " + sb.toString());
-//	      ticket.setName(sb.toString());
-//  	
+		// System.out.println(ticket.toString());
+		// System.out.println("Hi");
+		// StringBuilder sb = new StringBuilder();
+		// System.out.println(passenger.get());
+		// sb = sb.append(ticket.getName()).append(",").append(passenger.get());
+		// System.out.println("String: " + sb.toString());
+		// ticket.setName(sb.toString());
+		//
 		ticket.setPassengers(passenger.get());
-		 ticketRepository.saveAndFlush(ticket);
-	      
-	      System.out.println(ticket);
-	      System.out.println("name"+name);
-		
-		return new ModelAndView("redirect:/gettickets/"+name);
+		ticketRepository.saveAndFlush(ticket);
+
+		System.out.println(ticket);
+		System.out.println("name" + name);
+
+		return new ModelAndView("redirect:/gettickets/" + name);
 	}
 
+	// @DeleteMapping(value="/deleteticket/{id}", produces =
+	// {MediaType.APPLICATION_JSON_VALUE},consumes=
+	// {MediaType.APPLICATION_JSON_VALUE})
+	// public String deleteticket(@PathVariable Long id){
+	//
+	// System.out.println("Inside Get all Tickets");
+	//
+	// Optional<Ticket> t = ticketRepository.findById(id);
+	//
+	// if(t.isPresent()){
+	// ticketRepository.delete(t.get());
+	// return "Ticket SuccessFully Deleted";
+	// }
+	// else{
+	// return "Ticket not Found";
+	// }
+	//
+	// }
 
-//    @DeleteMapping(value="/deleteticket/{id}", produces = {MediaType.APPLICATION_JSON_VALUE},consumes= {MediaType.APPLICATION_JSON_VALUE})
-//    public String deleteticket(@PathVariable Long id){
-//
-//        System.out.println("Inside Get all Tickets");
-//
-//        Optional<Ticket> t = ticketRepository.findById(id);
-//
-//        if(t.isPresent()){
-//            ticketRepository.delete(t.get());
-//            return "Ticket SuccessFully Deleted";
-//        }
-//        else{
-//            return "Ticket not Found";
-//        }
-//
-//    }
-	
 	@RequestMapping("/edit/{id}")
 	public ModelAndView editTicket(@PathVariable(name = "id") Long id) {
 		ModelAndView mav = new ModelAndView("edit_ticket");
 		Optional<Ticket> ticket = ticketRepository.findById(id);
-		
-		if(ticket.isPresent()) {
+
+		if (ticket.isPresent()) {
 			System.out.println("Ticket Not found");
 		}
 		mav.addObject("ticket", ticket);
-		
+
 		return mav;
 	}
-	
+
 	@RequestMapping("/delete/{id}")
 	public ModelAndView deleteTicket(@PathVariable(name = "id") Long id) {
-		
+
 		Optional<Ticket> t = ticketRepository.findById(id);
-		
-		if(!t.isPresent()) {
+
+		if (!t.isPresent()) {
 			System.out.println("Ticket Not found");
 		}
 		ticketRepository.delete(t.get());
-		return new ModelAndView("redirect:/gettickets/"+t.get().getName());	
+		return new ModelAndView("redirect:/gettickets/" + t.get().getName());
 	}
 
 }
--- TicketingSystem/src/main/java/com/capstone/ticket/controller/UserController.java
+++ TicketingSystem/src/main/java/com/capstone/ticket/controller/UserController.java
@@ -11,178 +11,177 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 public class UserController {
 
-	 @Autowired
-	 UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
+    // @PostMapping("/signup")
+    // public String signup(@RequestBody User user) {
+    // System.out.println("Inside Signup");
+    // System.out.println(user.getName());
 
-    @PostMapping("/signup")
-    public String signup(@RequestBody User user) {
-        System.out.println("Inside Signup");
-        System.out.println(user.getName());
+    // Optional<User> existingUser =
+    // Optional.ofNullable(userRepository.findByName(user.getName()));
+    // if(existingUser.isPresent()){
+    // System.out.println("User already present. Please proceed to login");
+    // }
+    // userRepository.save(user);
 
-        Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(user.getName()));
-        if(existingUser.isPresent()){
-            System.out.println("User already present. Please proceed to login");
-        }
-        userRepository.save(user);
+    // return "Sign up successful";
+    // }
 
-        return "Sign up successful";
-    }
-
-//	@PostMapping("/login")
-//    public String login(@RequestBody User user) {
-//
-//        Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(user.getName()));
-//
-//
-//        if(!existingUser.isPresent()) {
-//            System.out.println("User does not exist");
-//            return "Cannot login. User is not registered, first sign up..!!";
-//        }else{
-//            if(!existingUser.get().getPassword().equals(user.getPassword())){
-//                System.out.println("User exists but password is wrong");
-//                return "Invalid Credentials!!";
-//            }
-//
-//        }
-//        System.out.println("User logged in successfully");
-//        return "User logged in successfully";
-//
-//
-//	}
-	
+    // @PostMapping("/login")
+    // public String login(@RequestBody User user) {
+    //
+    // Optional<User> existingUser =
+    // Optional.ofNullable(userRepository.findByName(user.getName()));
+    //
+    //
+    // if(!existingUser.isPresent()) {
+    // System.out.println("User does not exist");
+    // return "Cannot login. User is not registered, first sign up..!!";
+    // }else{
+    // if(!existingUser.get().getPassword().equals(user.getPassword())){
+    // System.out.println("User exists but password is wrong");
+    // return "Invalid Credentials!!";
+    // }
+    //
+    // }
+    // System.out.println("User logged in successfully");
+    // return "User logged in successfully";
+    //
+    //
+    // }
 
     @RequestMapping("/")
-	public ModelAndView viewHomePage() {
-		ModelAndView model = new ModelAndView("index1");
-		
-		return model;
-		
+    public ModelAndView viewHomePage() {
+        ModelAndView model = new ModelAndView("index1");
+
+        return model;
+
     }
-    
+
     @RequestMapping("/login")
-   	public ModelAndView loginUser() {
-   		User user = new User();
-   		ModelAndView model = new ModelAndView("login");
-   		model.addObject("user", user);
-   		
-   		return model;
-   	}
-    
+    public ModelAndView loginUser() {
+        User user = new User();
+        ModelAndView model = new ModelAndView("login");
+        model.addObject("user", user);
+
+        return model;
+    }
+
     @RequestMapping(value = "/loginuser", method = RequestMethod.POST)
-	public ModelAndView loginUser(@RequestParam("name") String name,@RequestParam("password") String password) {
-		
-    	Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
-		
-    	
-    	if(!existingUser.isPresent()) {
-            System.out.println("User does not exist");
-            
-        }else{
-            if(!existingUser.get().getPassword().equals(password)){
-                System.out.println("User exists but password is wrong");
-                
-            }
-
-        }
-        System.out.println("User logged in successfully");
-        
-		return new ModelAndView("redirect:gettickets/"+name);
-	}
-    
-    @RequestMapping("/signup")
-   	public ModelAndView signUp() {
-   		User user = new User();
-   		ModelAndView model = new ModelAndView("signup");
-   		model.addObject("user", user);
-   		
-   		return model;
-   	}
-    
-    @RequestMapping(value = "/signupuser", method = RequestMethod.POST)
-	public ModelAndView signUpUser(@RequestParam("name") String name,@RequestParam("password") String password, @RequestParam("address") String address,@RequestParam("contact") String contact) {
-		
-    	Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
-		
-
-    	if(existingUser.isPresent()) {
-            System.out.println("User exists, Please Login");
-            
-        }
-    	User user = new User();
-    	user.setName(name);
-    	user.setPassword(password);
-    	user.setAddress(address);
-    	user.setContact(contact);
-    	
-    	userRepository.save(user);
-		return new ModelAndView("redirect:gettickets/"+name);
-	}
-    
-    
-    @RequestMapping("/updateprofile")
-   	public ModelAndView update() {
-   		User user = new User();
-   		ModelAndView model = new ModelAndView("update");
-   		model.addObject("user", user);
-   		
-   		return model;
-   	}
-    
-    @RequestMapping(value = "/updateuser/{name}", method = RequestMethod.PUT)
-	public ModelAndView updateUser(@PathVariable String name,@RequestParam("name") String newname,@RequestParam("password") String password, @RequestParam("address") String address,@RequestParam("contact") String contact) {
-		
-    	Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
-		
-    	
-    	if(!existingUser.isPresent()) {
-            System.out.println("User does not exist");
-            
-        }
-    	existingUser.get().setName(newname);
-    	existingUser.get().setPassword(password);
-    	existingUser.get().setAddress(address);
-    	existingUser.get().setContact(contact);
-    	
-    	userRepository.saveAndFlush(existingUser.get());
-        System.out.println("User updated successfully");
-        
-		return new ModelAndView("redirect:gettickets/"+name);
-	}
-
-
-    @PutMapping("/updateprofile/{name}")
-    public String updateUser(@PathVariable String name, @RequestBody User user) {
+    public ModelAndView loginUser(@RequestParam("name") String name, @RequestParam("password") String password) {
 
         Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
 
         if (!existingUser.isPresent()) {
             System.out.println("User does not exist");
-            return "Cannot Update the user";
+
+        } else {
+            if (!existingUser.get().getPassword().equals(password)) {
+                System.out.println("User exists but password is wrong");
+
+            }
+
         }
+        System.out.println("User logged in successfully");
 
-        existingUser.get().setName(user.getName());
-        existingUser.get().setPassword(user.getPassword());
-        existingUser.get().setAddress(user.getAddress());
-        existingUser.get().setContact(user.getContact());
+        return new ModelAndView("redirect:gettickets/" + name);
+    }
 
-        System.out.println(existingUser.get());
+    @RequestMapping("/signup")
+    public ModelAndView signUp() {
+        User user = new User();
+        ModelAndView model = new ModelAndView("signup");
+        model.addObject("user", user);
+
+        return model;
+    }
+
+    @RequestMapping(value = "/signupuser", method = RequestMethod.POST)
+    public ModelAndView signUpUser(@RequestParam("name") String name, @RequestParam("password") String password,
+            @RequestParam("address") String address, @RequestParam("contact") String contact) {
+
+        Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
+
+        if (existingUser.isPresent()) {
+            System.out.println("User exists, Please Login");
+
+        }
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        user.setAddress(address);
+        user.setContact(contact);
+
+        userRepository.save(user);
+        return new ModelAndView("redirect:gettickets/" + name);
+    }
+
+    @RequestMapping("/updateprofile")
+    public ModelAndView update() {
+        User user = new User();
+        ModelAndView model = new ModelAndView("update");
+        model.addObject("user", user);
+
+        return model;
+    }
+
+    @RequestMapping(value = "/updateuser/{name}", method = RequestMethod.PUT)
+    public ModelAndView updateUser(@PathVariable String name, @RequestParam("name") String newname,
+            @RequestParam("password") String password, @RequestParam("address") String address,
+            @RequestParam("contact") String contact) {
+
+        Optional<User> existingUser = Optional.ofNullable(userRepository.findByName(name));
+
+        if (!existingUser.isPresent()) {
+            System.out.println("User does not exist");
+
+        }
+        existingUser.get().setName(newname);
+        existingUser.get().setPassword(password);
+        existingUser.get().setAddress(address);
+        existingUser.get().setContact(contact);
+
         userRepository.saveAndFlush(existingUser.get());
-        return "User Updated successfully";
+        System.out.println("User updated successfully");
+
+        return new ModelAndView("redirect:gettickets/" + name);
     }
 
+    // @PutMapping("/updateprofile/{name}")
+    // public String updateUser(@PathVariable String name, @RequestBody User user) {
 
-    @GetMapping(value="/getusers", produces = {MediaType.APPLICATION_JSON_VALUE},consumes= {MediaType.APPLICATION_JSON_VALUE})
-    public List<User> getUsers(){
+    // Optional<User> existingUser =
+    // Optional.ofNullable(userRepository.findByName(name));
 
-        System.out.println("Inside Get all Users");
-        System.out.println(userRepository.findAll());
-        return userRepository.findAll();
+    // if (!existingUser.isPresent()) {
+    // System.out.println("User does not exist");
+    // return "Cannot Update the user";
+    // }
 
-    }
+    // existingUser.get().setName(user.getName());
+    // existingUser.get().setPassword(user.getPassword());
+    // existingUser.get().setAddress(user.getAddress());
+    // existingUser.get().setContact(user.getContact());
+
+    // System.out.println(existingUser.get());
+    // userRepository.saveAndFlush(existingUser.get());
+    // return "User Updated successfully";
+    // }
+
+    // @GetMapping(value = "/getusers", produces = {
+    // MediaType.APPLICATION_JSON_VALUE }, consumes = {
+    // MediaType.APPLICATION_JSON_VALUE })
+    // public List<User> getUsers() {
+
+    // System.out.println("Inside Get all Users");
+    // System.out.println(userRepository.findAll());
+    // return userRepository.findAll();
+
+    // }
 
 }
