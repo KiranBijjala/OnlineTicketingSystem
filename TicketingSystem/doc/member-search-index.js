5929c850e99c298848cd48c0f2a085ae140896c3
Alejandro <deja@Alexs-MacBook-Air.local>
2021-06-23 10:45:00 -0700

    Added updates for having required input fields

---
+++ TicketingSystem/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.capstone.ticket","c":"TicketingSystemApplication","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.capstone.ticket","c":"TicketingSystemApplication","l":"TicketingSystemApplication()","u":"%3Cinit%3E()"}];updateSearchResults();
