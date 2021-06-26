514779feab046580bd3067040b50f2dca8b64faa
Alejandro Blanco <81883140+AlejandroCSC@users.noreply.github.com>
2021-06-25 17:56:04 -0700

    Updated more HTML files

---
+++ TicketingSystem/src/main/java/com/capstone/ticket/controller/ApplicationSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.capstone.ticket.controller;
+
+
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true,proxyTargetClass = true)
+public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests().antMatchers("/").permitAll().and()
+                .formLogin();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+    }
+
+    
+
+
+}
