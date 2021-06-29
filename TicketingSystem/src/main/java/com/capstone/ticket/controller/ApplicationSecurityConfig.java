59a2c60f64b907ba09b4cdd30298619f75bf6328
Alejandro Blanco <81883140+AlejandroCSC@users.noreply.github.com>
2021-06-28 20:25:02 -0700

    Updated a "logout" button to be in the NavBar

--- TicketingSystem/src/main/java/com/capstone/ticket/controller/ApplicationSecurityConfig.java
+++ TicketingSystem/src/main/java/com/capstone/ticket/controller/ApplicationSecurityConfig.java
@@ -1,33 +1,23 @@
 package com.capstone.ticket.controller;
 
-
-
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true,proxyTargetClass = true)
-public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
-
-    
+// @EnableGlobalMethodSecurity(prePostEnabled = true,proxyTargetClass = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests().antMatchers("/").permitAll().and()
-                .formLogin();
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().formLogin();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
 
     }
-
-    
-
 
 }
